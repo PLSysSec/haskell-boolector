d9285185d77538637b7952a944ef2ebdbf031ca4
Johannes Waldmann <waldmann@imn.htwk-leipzig.de>
2014-12-16 22:54:27 +0100

    start higher level interface (satchmo style)

--- API_Usage_Examples.hs
+++ API_Usage_Examples.hs
@@ -1,17 +1,36 @@
+import qualified Boolector.Foreign as BF
 import qualified Boolector as B
 
+import Control.Applicative
+
 main = do
-  b <- B.new
-  B.setOpt b "model_gen" 2
-  c <- B.unsignedInt b 35 8
-  x <- B.var b 8 "x" ;  y <- B.var b 8 "y"
-  p <- B.mul b x y ;  o <- B.umulo b x y
-  no <- B.not b o ;  e <- B.eq b c p
-  B.assert b =<< B.and b no e
-  one <- B.one b 8
-  B.assert b =<< B.ugt b x one
-  B.assert b =<< B.ugt b y one 
-  status <- B.sat b ; print status
-  s <- B.bvAssignment b x ; print s
-  t <- B.bvAssignment b y ; print t
+    higherlevel >>= print
+
+higherlevel = B.withBoolector $ do
+    c <- B.unsignedInt 35 8
+    x <- B.var 8 ; y <- B.var 8
+    p <- B.mul x y ; o <- B.umulo x y
+    no <- B.not o ; e <- B.eq c p
+    B.assert =<< B.and no e
+    one <- B.one 8
+    B.assert =<< B.ugt x one
+    B.assert =<< B.ugt y one
+    B.withSolution $ do
+        (,) <$> B.val x <*> B.val y
+
+lowlevel = do
+  b <- BF.new
+  BF.setOpt b "model_gen" 2
+  c <- BF.unsignedInt b 35 8
+  x <- BF.var b 8 "x" ;  y <- BF.var b 8 "y"
+  p <- BF.mul b x y ;  o <- BF.umulo b x y
+  no <- BF.not b o ;  e <- BF.eq b c p
+  BF.assert b =<< BF.and b no e
+  one <- BF.one b 8
+  BF.assert b =<< BF.ugt b x one
+  BF.assert b =<< BF.ugt b y one 
+  status <- BF.sat b ; print status
+  s <- BF.bvAssignment b x ; print s
+  t <- BF.bvAssignment b y ; print t
+
   
--- src/Boolector.hs
+++ src/Boolector.hs
@@ -1,8 +1,125 @@
-module Boolector
+{-# language GeneralizedNewtypeDeriving #-}
+{-# language NoMonomorphismRestriction #-}
 
-( module Boolector.Foreign 
-)  
+module Boolector
 
 where
 
-import Boolector.Foreign
+import qualified Boolector.Foreign as B
+
+import Control.Monad.State
+import Control.Applicative
+import Data.Char (isDigit)
+
+import Prelude hiding (read, not, and, or)
+import qualified Prelude
+
+data S =
+     S { btor :: ! B.Tor -- ^  solver instance
+       , next :: ! Int -- ^ for counting names
+       , solved :: ! Bool
+       }  
+
+newtype Boolector a = Boolector { un :: StateT S IO a }
+    deriving (Functor, Applicative, Monad, MonadState S, MonadIO )
+
+-- | build a formula, solve it, and process the solution.
+withBoolector :: (Boolector (Boolector a))
+         -> IO (Maybe a)
+withBoolector action = do
+    b <- B.new
+    B.setOpt b "model_gen" 2
+    let s0 = S { btor = b , next = 0, solved = False }
+    cont <- evalStateT (un action) s0
+    status <- B.sat b
+    case status of
+        B.SAT -> do
+            Just <$> evalStateT (un cont) s0 { solved = True }
+        _ -> return Nothing
+
+-- | run the action after a solution was found.
+withSolution action = return action
+
+-- | create a fresh integer unknown. argument: bit width
+var :: Int -> Boolector B.Node
+var width = do
+    s <- get
+    put $ s { next = succ $ next s }
+    wrap2 B.var width $ "var" ++ (show $ next s)
+
+-- | get the value for this variable.    
+val :: B.Node -> Boolector Integer
+val n = do
+    s <- get
+    when (Prelude.not $ solved s) $ error "Boolector.value: may only occur after withSolution"
+    a <- wrap1 B.bvAssignment n
+    when (Prelude.not $ all isDigit a) $ error $ "Boolector.value: not numeric: " ++ a
+    return $ foldl ( \ n c -> 2 * n + Prelude.read [c] ) 0 a
+
+wrap0 action = do
+    s <- get ; liftIO $ action ( btor s )
+wrap1 action x1 = do
+    s <- get ; liftIO $ action ( btor s ) x1
+wrap2 action x1 x2 = do
+    s <- get ; liftIO $ action ( btor s ) x1 x2
+wrap3 action x1 x2 x3 = do
+    s <- get ; liftIO $ action ( btor s ) x1 x2 x3
+
+assert = wrap1 B.assert
+false = wrap0 B.false
+zero = wrap1 B.zero
+true = wrap0 B.true
+ones = wrap1 B.ones
+one = wrap1 B.one
+unsignedInt = wrap2 B.unsignedInt
+int = wrap2 B.int
+not = wrap1 B.not
+neg = wrap1 B.neg
+redor = wrap1 B.redor
+redxor = wrap1 B.redxor
+redand = wrap1 B.redand
+slice = wrap3 B.slice
+uext = wrap2 B.uext
+sext = wrap2 B.sext
+implies = wrap2 B.implies
+iff = wrap2 B.iff
+xor = wrap2 B.xor
+xnor = wrap2 B.xnor
+and = wrap2 B.and
+nand = wrap2 B.nand
+or = wrap2 B.or
+nor = wrap2 B.nor
+eq = wrap2 B.eq
+ne = wrap2 B.ne
+add = wrap2 B.and
+uaddo = wrap2 B.uaddo
+saddo = wrap2 B.saddo
+mul = wrap2 B.mul
+umulo = wrap2 B.umulo
+smulo = wrap2 B.smulo
+ult = wrap2 B.ult
+slt  = wrap2 B.slt 
+ulte  = wrap2 B.ulte 
+slte  = wrap2 B.slte 
+ugt  = wrap2 B.ugt 
+sgt  = wrap2 B.sgt 
+ugte = wrap2 B.ugte
+sll  = wrap2 B.sll 
+srl  = wrap2 B.srl 
+sra  = wrap2 B.sra 
+rol  = wrap2 B.rol 
+ror  = wrap2 B.ror 
+sub  = wrap2 B.sub 
+usubo = wrap2 B.usubo
+ssubo = wrap2 B.ssubo
+udiv  = wrap2 B.udiv 
+sdiv  = wrap2 B.sdiv 
+sdivo = wrap2 B.sdivo
+urem  = wrap2 B.urem 
+srem  = wrap2 B.srem 
+smod  = wrap2 B.smod 
+concat  = wrap2 B.concat 
+read  = wrap2 B.read 
+write = wrap3 B.write
+cond = wrap3 B.cond
+
