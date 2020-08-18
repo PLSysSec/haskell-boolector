54faeb06e0f8cd28389bfc1a8f10b84084e0bf0d
Deian Stefan <deian@cs.ucsd.edu>
2020-08-18 14:41:16 -0700

    Revert "Pull in boolector locally"
    This reverts commit ba67797006f0372f5e4c9b9ceab7aec10d5a255b.

--- Setup.hs
+++ Setup.hs
@@ -1,14 +1,2 @@
 import Distribution.Simple
-import Distribution.Types.HookedBuildInfo
-import Distribution.Simple.Setup
-import Distribution.Simple.Utils (rawSystemExit)
-
-main :: IO ()
-main = defaultMainWithHooks simpleUserHooks {
-          preBuild = preBuildApron
-       }
-
-preBuildApron :: Args -> BuildFlags -> IO HookedBuildInfo
-preBuildApron args flags = do
-  rawSystemExit (fromFlag $ buildVerbosity flags) "env" ["make", "deps/boolector-dist"]
-  preBuild simpleUserHooks args flags
+main = defaultMain
